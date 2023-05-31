11693831c5b283f5b2cf364fc90b962c3658e1e7
Courtin <benjidu490@gmail.com>
2023-05-31 18:15:06 +0200

    affichage photographers page d'accueil et recuperation photographers sur la page photograhers grace a l'id dans l'url

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,16 +1,29 @@
-function photographerFactory(data) {
-    const { name, portrait } = data;
+export default function photographerFactory(data) {
+    const { id, name, portrait, city, country , tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+        const a = document.createElement( 'a' );
+        a.setAttribute('href', `photographer.html?id=${id}`);
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
+        img.setAttribute('src', picture);
+        img.setAttribute('alt', 'Portrait du photographe');
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
-        article.appendChild(img);
+        const h3 = document.createElement( 'h3' );
+        h3.textContent = country + ', ' + city;
+        const p = document.createElement( 'p' );
+        p.textContent = tagline;
+        const span = document.createElement( 'span' );
+        span.textContent = price + '€/jour';
+        article.appendChild(a);
+        a.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(h3);
+        article.appendChild(p);
+        article.appendChild(span);
         return (article);
     }
     return { name, picture, getUserCardDOM }
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,16 @@
 //Mettre le code JavaScript lié à la page photographer.html
+import getPhotographers from "./../models/photographer.js";
+
+function getPhotographerIdFromUrl(){
+    let params = (new URL(document.location)).searchParams;
+    return params.get('id'); 
+}
+async function init() {
+    // Récupère les datas des photographes
+    const { photographers } = await getPhotographers();
+    const result = photographers.find(photographer => photographer.id == getPhotographerIdFromUrl());
+    console.log(photographers);
+    console.log(result);
+};
+
+init();
